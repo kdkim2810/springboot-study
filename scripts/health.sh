6e10d2fcc99a17dd450a4ac94aac0a5e24fe9643
kdkim2810 <kdkim2810@gmail.com>
2020-03-25 19:41:00 +0900

    Modify health.sh For Typo

--- scripts/health.sh
+++ scripts/health.sh
@@ -15,7 +15,7 @@ sleep 10
 for RETRY_COUNT in {1..10}
 do
   RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
-  UP_COUNT=$(echo ${REPONSE} | grep 'real' | wc -1)
+  UP_COUNT=$(echo ${REPONSE} | grep 'real' | wc -l)
 
   if [ ${UP_COUNT} -ge 1 ]
   then # $up_count >= 1 ("real" 문자열이 있는지 검증)
@@ -24,7 +24,7 @@ do
     break
   else
     echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
-    echo "> Health check: ${REPONSE}"
+    echo "> Health check: ${RESPONSE}"
   fi
 
   if [ ${RETRY_COUNT} -eq 10 ]
