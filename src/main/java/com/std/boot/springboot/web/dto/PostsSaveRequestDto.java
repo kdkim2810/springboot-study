a5a7e6e6f25302b3cbee56033946bea9ac70ee8d
kdkim2810 <kdkim2810@gmail.com>
2020-03-25 19:14:51 +0900

    Add Scripts & Config For Nonstop Deploy

--- src/main/java/com/std/boot/springboot/web/dto/PostsSaveRequestDto.java
+++ src/main/java/com/std/boot/springboot/web/dto/PostsSaveRequestDto.java
@@ -5,12 +5,22 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Getter
 @NoArgsConstructor
 public class PostsSaveRequestDto {
+
+    @NotEmpty(message = "{post.title.empty}")
     private String title;
+
+    @NotEmpty(message = "내용은 필수값입니다.")
     private String content;
+
+    @Size(min = 2, max = 10)
     private String author;
+
     @Builder
     public PostsSaveRequestDto(String title, String content, String author) {
         this.title = title;
